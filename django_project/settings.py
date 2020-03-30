fcdaf93b70988f0cbcdc057286c80fb075af6e8c
Prithviraj <prithvirajp2899@gmail.com>
2020-03-30 13:51:49 +0530

    secret key3

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['django_project_secretkey']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['prithviblog.herokuapp.com','127.0.0.1']
 
